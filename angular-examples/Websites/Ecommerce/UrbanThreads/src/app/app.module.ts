eb012b77ba68af2752ec8a875a45c2b50326766e
Drackus001 <Satyam.Agarwal.IN@gmail.com>
2023-07-27 14:47:34 +0530

    initial component created

--- angular-examples/Websites/Ecommerce/UrbanThreads/src/app/app.module.ts
+++ angular-examples/Websites/Ecommerce/UrbanThreads/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SubscribeToEmailComponent } from './components/subscribe-to-email/subscribe-to-email.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { TestimonialComponent } from './components/testimonial/testimonial.component';
+import { ArrivalComponent } from './components/arrival/arrival.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, FooterComponent, HeaderComponent, SubscribeToEmailComponent, AboutUsComponent, TestimonialComponent, ArrivalComponent, ContactUsComponent, HeroComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ angular-examples/Websites/Ecommerce/UrbanThreads/src/app/components/subscribe-to-email/subscribe-to-email.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscribe-to-email',
+  templateUrl: './subscribe-to-email.component.html',
+  styleUrls: ['./subscribe-to-email.component.scss']
+})
+export class SubscribeToEmailComponent {
+
+}
